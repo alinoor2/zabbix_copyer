2c6715810b25b9ae2449958fc4162e782cf621f9
alinoor2 <ali.noorbakhsh2@gmail.com>
2022-04-20 18:00:32 +0430

    change classes for best run program get items from ref and cpy zabbix Compares the two configurations and output with 'YES' tag means not in cpy zabbix or 'NO' tag means not in cpy zabbix insert to database complete # TODO create api request for config second zabbix server

--- main.py
+++ main.py
@@ -16,6 +16,11 @@ if __name__ == '__main__':
     comp_items_dic = comp_items.out_dic
     with open('log/out_dic.yaml', "w") as f:
         f.write(yaml.dump(comp_items_dic, default_flow_style=False))
+    db_ref = db.DataBaseLoader(conf.ZABBIX_REF_ADDRESS, host_ref)
+    db_ref.insert_json_to_database()
+    db_cpy = db.DataBaseLoader(conf.ZABBIX_CPY_ADDRESS, host_cpy)
+    db_cpy.insert_json_to_database()
+
     end = time.time()
     total_time = end - start
     print("\n"+ str(total_time))
--- src/db_worker.py
+++ src/db_worker.py
@@ -1,59 +1,105 @@
 import sqlite3
+import src.config as conf
+
 
 class DataBaseLoader(object):
-    def __init__(self):
-        self.zabbix_db_name = self.zabbix_ip.split('.')[2] + '_' + self.zabbix_ip.split('.')[3]
-        self.dbname = "log/output_{}db.sqlite".format(self.zabbix_db_name)
+    def __init__(self, zabbix_address, hosts_dic):
+        self.output_data = None
+        self.zabbix_db_name = "dbs/output_{}db.sqlite".format(zabbix_address.split('.')[2] + '_' +
+                                                              zabbix_address.split('.')[3].split('/')[0])
+        self.hosts_dic = hosts_dic
 
     def insert_json_to_database(self):
-        
-        connect = sqlite3.connect(self.dbname)
+        connect = sqlite3.connect(self.zabbix_db_name)
         cursor = connect.cursor()
         try:
-            if self.zabbix_type == "ref":
-                cursor.execute('CREATE TABLE hosts_name (hostname VARCHAR, hostid VARCHAR, stat VARCHAR)')
-            elif self.zabbix_type == "cpy":
-                cursor.execute('CREATE TABLE hosts_name (hostname VARCHAR, hostid VARCHAR)')
-            else:
-                pass
-            if self.zabbix_type == "ref":
-                cursor.execute('CREATE TABLE host_detailes (hostid VARCHAR, itemid VARCHAR, itemname VARCHAR, itemkey VARCHAR, iteminterval VARCHAR, \
-                    itemunit VARCHAR, itemstatus VARCHAR, itemvaluetype VARCHAR, stat VARCHAR)')
-            elif self.zabbix_type == "cpy":
-                cursor.execute('CREATE TABLE host_detailes (hostid VARCHAR, itemid VARCHAR, itemname VARCHAR, itemkey VARCHAR, iteminterval VARCHAR, \
-                    itemunit VARCHAR, itemstatus VARCHAR, itemvaluetype VARCHAR)')
-            else:
-                pass
-
+            cursor.execute('CREATE TABLE hosts_name (hostname VARCHAR, hostid VARCHAR, stat VARCHAR)')
+            cursor.execute('CREATE TABLE host_detailes (hostid VARCHAR, itemid VARCHAR, itemname VARCHAR, '
+                           'itemkey VARCHAR, iteminterval VARCHAR, itemunit VARCHAR, itemstatus VARCHAR, '
+                           'itemvaluetype VARCHAR, stat VARCHAR)')
         except:
             pass
-        for host in self.hosts:
-            command = 'INSERT INTO hosts_name (hostname, hostid) SELECT "{0}", "{1}" WHERE NOT EXISTS(SELECT 1 FROM hosts_name WHERE hostname = \
-                "{0}" AND hostid =  "{1}")'.format(host, self.hosts[host]["hostid"])
+        for host in self.hosts_dic:
+            command = 'INSERT INTO hosts_name (hostname, hostid) SELECT "{0}", ' \
+                      '"{1}" WHERE NOT EXISTS(SELECT 1 FROM hosts_name WHERE hostname = '\
+                      '"{0}" AND hostid =  "{1}")'.format(host, self.hosts_dic[host]["hostid"])
             cursor.execute(command)
-            for item in self.hosts[host]["items"]:
-                # print(item)
+            for item in self.hosts_dic[host]["items"]:
                 item.replace('"', '')
-                if self.zabbix_type == "ref":
-                    command = 'INSERT INTO host_detailes (hostid, itemid, itemname, itemkey, iteminterval, itemunit, itemstatus, itemvaluetype, stat) SELECT \
-                        "{0}", "{1}", "{2}", "{3}", "{4}", "{5}", "{6}", "{7}" , "{8}" WHERE NOT EXISTS(SELECT 1 FROM host_detailes WHERE hostid =  "{0}" AND \
-                            itemname = "{2}")'.format(self.hosts[host]["hostid"], self.hosts[host]["items"][item]["itemid"], item.replace('"', ''), \
-                                self.hosts[host]["items"][item]["itemkey"].replace('"', ''), self.hosts[host]["items"][item]["iteminterval"], \
-                                    self.hosts[host]["items"][item]["itemunit"], self.hosts[host]["items"][item]["itemstatus"], \
-                                        self.hosts[host]["items"][item]["itemvaluetype"], self.hosts[host]["items"][item]["stat"])
-                else:
-                    command = 'INSERT INTO host_detailes (hostid, itemid, itemname, itemkey, iteminterval, itemunit, itemstatus, itemvaluetype) SELECT \
-                        "{0}", "{1}", "{2}", "{3}", "{4}", "{5}", "{6}", "{7}"  WHERE NOT EXISTS(SELECT 1 FROM host_detailes WHERE hostid =  "{0}" AND \
-                            itemname = "{2}")'.format(self.hosts[host]["hostid"], self.hosts[host]["items"][item]["itemid"], item.replace('"', ''), \
-                                self.hosts[host]["items"][item]["itemkey"].replace('"', ''), self.hosts[host]["items"][item]["iteminterval"], \
-                                    self.hosts[host]["items"][item]["itemunit"], self.hosts[host]["items"][item]["itemstatus"], \
-                                        self.hosts[host]["items"][item]["itemvaluetype"])
-                cursor.execute(command)
+                command = 'INSERT INTO host_detailes (hostid, itemid, itemname, itemkey, iteminterval, itemunit, ' \
+                          'itemstatus, itemvaluetype, stat) SELECT "{0}", "{1}", "{2}", "{3}", "{4}", "{5}", "{6}", ' \
+                          '"{7}" , "{8}" WHERE NOT EXISTS(SELECT 1 FROM host_detailes WHERE hostid =  "{0}" AND ' \
+                          'itemname = "{2}")'.format(self.hosts_dic[host]["hostid"],
+                                                     self.hosts_dic[host]["items"][item]["itemid"], item.replace('"', ''),
+                                                     self.hosts_dic[host]["items"][item]["itemkey"].replace('"', ''),
+                                                     self.hosts_dic[host]["items"][item]["iteminterval"],
+                                                     self.hosts_dic[host]["items"][item]["itemunit"],
+                                                     self.hosts_dic[host]["items"][item]["itemstatus"],
+                                                     self.hosts_dic[host]["items"][item]["itemvaluetype"],
+                                                     self.hosts_dic[host]["items"][item]["stat"])
+                try:
+                    cursor.execute(command)
+                except:
+                    pass
         connect.commit()
 
+    # def insert_json_to_database(self):
+    #     connect = sqlite3.connect(self.zabbix_db_name_cpy)
+    #     cursor = connect.cursor()
+    #     try:
+    #         if self.zabbix_type == "ref":
+    #             cursor.execute('CREATE TABLE hosts_name (hostname VARCHAR, hostid VARCHAR, stat VARCHAR)')
+    #         elif self.zabbix_type == "cpy":
+    #             cursor.execute('CREATE TABLE hosts_name (hostname VARCHAR, hostid VARCHAR)')
+    #         else:
+    #             pass
+    #         if self.zabbix_type == "ref":
+    #             cursor.execute('CREATE TABLE host_detailes (hostid VARCHAR, itemid VARCHAR, itemname VARCHAR, itemkey VARCHAR, iteminterval VARCHAR, \
+    #                 itemunit VARCHAR, itemstatus VARCHAR, itemvaluetype VARCHAR, stat VARCHAR)')
+    #         elif self.zabbix_type == "cpy":
+    #             cursor.execute('CREATE TABLE host_detailes (hostid VARCHAR, itemid VARCHAR, itemname VARCHAR, itemkey VARCHAR, iteminterval VARCHAR, \
+    #                 itemunit VARCHAR, itemstatus VARCHAR, itemvaluetype VARCHAR)')
+    #         else:
+    #             pass
+    #
+    #     except:
+    #         pass
+    #     for host in self.hosts:
+    #         command = 'INSERT INTO hosts_name (hostname, hostid) SELECT "{0}", "{1}" WHERE NOT EXISTS(SELECT 1 FROM hosts_name WHERE hostname = \
+    #             "{0}" AND hostid =  "{1}")'.format(host, self.hosts[host]["hostid"])
+    #         cursor.execute(command)
+    #         for item in self.hosts[host]["items"]:
+    #             # print(item)
+    #             item.replace('"', '')
+    #             if self.zabbix_type == "ref":
+    #                 command = 'INSERT INTO host_detailes (hostid, itemid, itemname, itemkey, iteminterval, itemunit, itemstatus, itemvaluetype, stat) SELECT \
+    #                     "{0}", "{1}", "{2}", "{3}", "{4}", "{5}", "{6}", "{7}" , "{8}" WHERE NOT EXISTS(SELECT 1 FROM host_detailes WHERE hostid =  "{0}" AND \
+    #                         itemname = "{2}")'.format(self.hosts[host]["hostid"],
+    #                                                   self.hosts[host]["items"][item]["itemid"],
+    #                                                   item.replace('"', ''), \
+    #                                                   self.hosts[host]["items"][item]["itemkey"].replace('"', ''),
+    #                                                   self.hosts[host]["items"][item]["iteminterval"], \
+    #                                                   self.hosts[host]["items"][item]["itemunit"],
+    #                                                   self.hosts[host]["items"][item]["itemstatus"], \
+    #                                                   self.hosts[host]["items"][item]["itemvaluetype"],
+    #                                                   self.hosts[host]["items"][item]["stat"])
+    #             else:
+    #                 command = 'INSERT INTO host_detailes (hostid, itemid, itemname, itemkey, iteminterval, itemunit, itemstatus, itemvaluetype) SELECT \
+    #                     "{0}", "{1}", "{2}", "{3}", "{4}", "{5}", "{6}", "{7}"  WHERE NOT EXISTS(SELECT 1 FROM host_detailes WHERE hostid =  "{0}" AND \
+    #                         itemname = "{2}")'.format(self.hosts[host]["hostid"],
+    #                                                   self.hosts[host]["items"][item]["itemid"],
+    #                                                   item.replace('"', ''), \
+    #                                                   self.hosts[host]["items"][item]["itemkey"].replace('"', ''),
+    #                                                   self.hosts[host]["items"][item]["iteminterval"], \
+    #                                                   self.hosts[host]["items"][item]["itemunit"],
+    #                                                   self.hosts[host]["items"][item]["itemstatus"], \
+    #                                                   self.hosts[host]["items"][item]["itemvaluetype"])
+    #             cursor.execute(command)
+    #     connect.commit()
+
     def read_from_database(self):
-        connect = sqlite3.connect(self.dbname)
+        connect = sqlite3.connect(self.zabbix_db_name)
         cursor = connect.cursor()
         cursor.execute('SELECT * FROM host_detailes')
-        data = cursor.fetchall()
-        print(data)
+        self.output_data = cursor.fetchall()
+
